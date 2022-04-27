44ba3b4ff740b1b55db5ace6800a2d134b7033a4
jicu mollick <jikumollick82@gmail.com>
2022-04-27 19:21:08 +0600

    adding steps

---
+++ __server_deploys.js
@@ -0,0 +1,15 @@
+/* 
+
+One Time For Each Project 
+1. create heroku account
+2.veryfy email
+3.install heroku cli
+4.heroku login
+
+
+
+For each Project One time
+1.
+
+
+*/
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -10,6 +10,23 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+function veryfyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.le9xm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -23,11 +40,20 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    client.connect();
+    await client.connect();
     const serviceCollection = client.db("geniusCar").collection("service");
+    const orderCollection = client.db("geniusCar").collection("order");
 
     console.log("database connected");
 
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+    // Services API
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
@@ -55,6 +81,27 @@ async function run() {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // order Collection API
+
+    app.get("/order", veryfyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      } else {
+        res.status(403).send({ message: "Forbidden Access" });
+      }
+    });
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
       res.send(result);
     });
   } finally {
