c20fa596aadc131ff915a6e2bc978e42dbf4e0f9
jicu mollick <jikumollick82@gmail.com>
2022-04-27 19:45:05 +0600

    hero api created

--- __server_deploys.js
+++ __server_deploys.js
@@ -7,9 +7,21 @@ One Time For Each Project
 4.heroku login
 
 
-
+================================================================================
 For each Project One time
-1.
 
+1.heroku create
+2.make sure you git add , git commit , git push
+3. git push heroku main
+4. got to heroku dashboard > current projects > settings > Reveal config vars 
+5. copy paste config vars from your .env file 
+6. Make sure you have whitelisted all the ip addresses to access mongodb
+============================================================================
+Update Server with new Changes 
 
+1.Make Changes 
+2.make sure you git add , git commit , git push
+3. git push heroku main
+
+=====================================================================
 */
--- index.js
+++ index.js
@@ -112,6 +112,9 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Running Genius Cars server");
 });
+app.get("/hero", (req, res) => {
+  res.send("I am hero vaiya");
+});
 
 app.listen(port, () => {
   console.log("Listening to port", port);
